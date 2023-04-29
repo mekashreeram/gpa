e72924bd7ae8561f65f63049adabe7a19ee2758e
mekashreeram <21131a1223@gvpce.ac.in>
2023-04-29 12:01:28 -0700

    Update settings.py

--- gpa/settings.py
+++ gpa/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yax_+a__t8scaac=#z&v-gha(p#v0td&!)+&xf^#k-=dqv(&j!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
